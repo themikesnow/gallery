d333602a3b2a0dad8fc001871fcef45db12738f6
Miguel A. Nieves Acevedo <miguel.a.nieves-acevedo@hp.com>
2017-07-06 13:15:04 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -10,13 +10,10 @@ var host = process.env.HOST || "127.0.0.1";
 
 
 app.get('*', function(req, res) {
-  // res.sendFile(__dirname + '/public/index.html');
   res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.use(express.static(__dirname + '/dist'));
-// app.use('/api', proxy({target: 'http://localhost:9007', changeOrigin: true, pathRewrite: {'^/api' : '/rest'}}));
-// app.use('/authentication', proxy({target: 'http://localhost:9007', changeOrigin: true, pathRewrite: {'^/authentication' : '/login'}}));
 
 app.listen(port);
 
